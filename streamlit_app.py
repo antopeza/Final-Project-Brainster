b4ddd09487a4705866553edb15a45165168a8bf1
antopeza <63090323+antopeza@users.noreply.github.com>
2022-10-20 13:22:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ Original file is located at
 
 #!pip install streamlit
 
-#import streamlit as st
-#import pandas as pd
+import streamlit as st
+import pandas as pd
 
 st.write("""
 # SIMPLE TEXT SIMILARITY APP 
