12edfb2e4947160163cebbf041a2e054d48fb0dd
antopeza <63090323+antopeza@users.noreply.github.com>
2022-10-20 19:40:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,7 +94,7 @@ def main():
         word_2 = st.text_area('Please write your sentence 2')
         word_2_m = [sent.lower().split(" ") for sent in word_2]
         
-    st.json(word_1_m)
+    st.success(word_1_m)
         
         #click = st.button('click me')
         
