bd2cbc606013391b0132ed7db44f2367bccf125e
antopeza <63090323+antopeza@users.noreply.github.com>
2022-10-20 20:03:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,24 +26,27 @@ def main():
         sentence_1 = sentence_1.lower().strip()
         sentence_1 = re.sub(r'[^a-z0-9\s]', '', sentence_1)  # removing all caracters that are not alpha numeric
         sentence_1 = sentence_1.translate(str.maketrans('', '', string.punctuation))  # remove punctuation
+        
+        st.success(sentence_1
         #sentence_1 = remove_stopwords(sentence_1)
         
-        vectorizer = CountVectorizer()
-        X = vectorizer.fit_transform(sentence_1)
-        arr = X.toarray()
-        heatmap = create_heatmap(cosine_similarity(arr))
+#         vectorizer = CountVectorizer()
+#         X = vectorizer.fit_transform(sentence_1)
+#         arr = X.toarray()
+#         heatmap = create_heatmap(cosine_similarity(arr))
         
         
-        st.success(heatmap)
-#      with col2:
-#         sentence_2 = st.text_area('Please write your sentence 1')
-#         sentence_2 = sentence_2.lower().strip()
-#         sentence_2 = re.sub(r'[^a-z0-9\s]', '', sentence_2)  # removing all caracters that are not alpha numeric
+#         st.success(heatmap)
+
+     with col2:
+        sentence_2 = st.text_area('Please write your sentence 1')
+        sentence_2 = sentence_2.lower().strip()
+        sentence_2 = re.sub(r'[^a-z0-9\s]', '', sentence_2)  # removing all caracters that are not alpha numeric
     
-#         sentence_2 = sentence_2.translate(str.maketrans('', '', string.punctuation))  # remove punctuation
-#         #sentence_1 = remove_stopwords(sentence_1)
+        sentence_2 = sentence_2.translate(str.maketrans('', '', string.punctuation))  # remove punctuation
+        #sentence_1 = remove_stopwords(sentence_1)
         
-#         st.success(sentence_2)
+        st.success(sentence_2)
         
     
 
