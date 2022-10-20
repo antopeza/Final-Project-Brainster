0c5232d59610b40ab12a3d47c249b1fbdabfc8ec
antopeza <63090323+antopeza@users.noreply.github.com>
2022-10-20 16:43:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,8 @@ Compare two sentences and check it out!
 
 col1, col2 = st.columns(2)
 with col1:
-    word_1 = st.text_input('sentence 1', 'shirt')
+    word_1 = st.text_area('Please write your sentence 1')
 with col2:
-    word_2 = st.text_input('sentence 2', 'jeans')
+    word_2 = st.text_area('Please write your sentence 2')
+
+    
